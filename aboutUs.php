136b385530778d7119ed5a1b0da7e5df72a485f4
StudentTechsima <techsimatraining@gmail.com>
2024-01-27 12:00:24 +0530

    Initial commit

---
+++ aboutUs.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AboutUs</title>
+    <link rel="stylesheet" href="css/aboutUs.css">
+    <link rel="stylesheet" href="css/all.css">
+</head>
+<body>
+    <script src="js/fontawesome.min.js"></script>
+    <section class="firstSeciton">
+        <div class="firstSeciton-box"><h1>AboutUs</h1></div>
+    </section>
+    
+    <section>
+        <div class="container">
+            <h3>Guaranteed Success</h3>
+            <h1>Our Services</h1>
+            <p>Proin ac lobortis arcu, a vestibulum augue. Vivamus ipsum neque, facilisis vel mollis vitae, mollis nec ante. Quisque aliquam dictum condim.</p>
+            <div class="Service">
+                <div class="services">
+                    <div class="services-part">
+                        <div class="image">
+                            <a href="#"><img src="images/mail.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="services-part1">
+                    <h2><a href="#">Notification & Emails</a></h2>
+                        <p class="noti-mail">Lorem ipsum dolor sit ametcon, sectetur adipiscing elit. Phasellus vehic sagittis euismod.</p>
+                    </div>
+                </div>
+                <div class="services">
+                    <div class="services-part">
+                        <div class="image">
+                            <a href="#"><img src="images/award.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="services-part1">
+                        <a href="#"><h2><a href="#">Certification</a></h2>
+                        <p class="noti-mail">Lorem ipsum dolor sit ametcon, sectetur adipiscing elit. Phasellus vehic sagittis euismod.</p>
+                    </div>
+                </div>
+                <div class="services">
+                    <div class="services-part">
+                        <div class="image">
+                            <a href="#"><img src="images/campus.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="services-part1">
+                        <h2><a href="#">Europe Campus</a></h2>
+                        <p class="noti-mail">Lorem ipsum dolor sit ametcon, sectetur adipiscing elit. Phasellus vehic sagittis euismod.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <h3>Research & Pedagogy</h3>
+            <h1>How It Works?</h1>
+            <p>Proin ac lobortis arcu, a vestibulum augue. Vivamus ipsum neque, facilisis vel mollis vitae, mollis nec ante. Quisque aliquam dictum condim.</p>
+            <div class="research-box"></div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School Management</title>
+    <link rel="icon" href="images/managment-software-2048x2048.png">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/all.css">
+</head>
+<body>
+    <script src="js/fontawesome.min.js"></script>
+    <!-------------------------------------Header Section Start--------------------------------------------->
+    <header>
+        <div class="contect"><a href=""><i class="fa-solid fa-phone fa-spin"></i>&nbsp;Helpline:+91 7800996687</a></div>
+        <div class="contect1"><a href=""><i class="fa-regular fa-envelope"></i>&nbsp;schoolmanagement@gmail.com</a></div>
+        <div class="contect2">
+            <div class="social-contect facebook"><span><a href="" class="social-contact"><i class="fa-brands fa-facebook"></i></a></span></div>
+            <div class="social-contect instagram"><span><a href="" class="social-contact"><i class="fa-brands fa-instagram"></i></a></span></div>
+            <div class="social-contect youtube"><span><a href="" class="social-contact"><i class="fa-brands fa-youtube"></i></a></span></div>
+        </div>
+        <div class="contect3"><a href="sing-up.html">LOGIN</a></div>
+    </header>
+    <!-----------------------------Header Section End-------------------------------------->
+
+    <!------------------------------Logo Section Start-------------------------------->
+    <section>
+        <div class="container">
+            <div class="logo">
+                <img src="images/managment-software-2048x2048.png" alt="">
+            </div>
+            <div class="schoolname">
+                <h1>SCHOOL MANAGEMENT</h1><br>
+                <h2>स्कूल मैनेजमेंट </h2>
+            </div>
+            <div class="logo1">
+                <img src="images/588030_87d9a.gif" alt="">
+            </div>
+        </div>
+    </section>
+    <!--------------------------------Logo Section End---------------------------------->
+
+    <!----------------------------------Landing Section Start----------------------------------->
+    <section>
+        <div class="manubar">
+            <ul>
+                <a href="" class="home"><li class="link home">HOME</li></a>
+                <a href=""><li class="link">SCHOOLS</li></a>
+               
+                <a href=""><li class="link">REGISTERS</li></a>
+                <a href="department.html"><li class="link">TEACHERS</li></a>
+                <a href=""><li class="link">NEWS</li></a>
+                <a href=""><li class="link">COURSES</li></a>
+                <a href=""><li class="link">ABOUT US</li></a>
+                <a href=""><li class="link">GALLERY</li></a>
+                <a href=""><li class="link">FAQ</li></a>
+                <a href=""><li class="link">CONTACT US</li></a>
+            </ul>
+            <div class="manubar1"><a href=""><div class="manudiv"></div></a>
+        </div>
+        </div>
+    </section>    
+    <!----------------------------------Landing Section End----------------------------------->
+
+<!-- --------------------------------------Slider Section Start------------------------------------------- -->
+<section class="slider-section">
+    <div class="slide">
+        <img src="images/college1.jpg" alt="" id="img">
+    </div>
+</section>
+<!-- --------------------------------------Slider Section End------------------------------------------- -->
+
+
+    <!-- -------------------------Notifications / Announcements Start----------------------------------- -->
+    <section>
+        <div class="announcements">
+            <h1>Notifications / <b> Announcements</b></h1>
+            <div class="notic">
+                <marquee direction="up">
+                    <a href=""><h2>Techsima 1 :-  </h2></a>
+                    <a href=""><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, blanditiis. Ullam possimus deleniti iusto voluptatibus iure ducimus quas saepe, maxime voluptates dicta. Harum, corporis necessitatibus? Quibusdam suscipit nulla, iste iusto fugiat id alias totam atque commodi est facilis vitae error reprehenderit ratione, veniam autem, cum doloribus earum animi. Nesciunt, atque!</p><br><br></a>
+                    <a href=""><h2>Techsima 2 :-  </h2></a>
+                    <a href=""><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, blanditiis. Ullam possimus deleniti iusto voluptatibus iure ducimus quas saepe, maxime voluptates dicta. Harum, corporis necessitatibus? Quibusdam suscipit nulla, iste iusto fugiat id alias totam atque commodi est facilis vitae error reprehenderit ratione, veniam autem, cum doloribus earum animi. Nesciunt, atque!</p></a><br><br>
+                    <a href=""><h2>Techsima 3 :-  </h2></a>
+                    <a href=""><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, blanditiis. Ullam possimus deleniti iusto voluptatibus iure ducimus quas saepe, maxime voluptates dicta. Harum, corporis necessitatibus? Quibusdam suscipit nulla, iste iusto fugiat id alias totam atque commodi est facilis vitae error reprehenderit ratione, veniam autem, cum doloribus earum animi. Nesciunt, atque!</p></a><br><br>
+               
+                </marquee>
+            </div>
+        </div>
+    </section>
+    <!-- ---------------------------- Notifications / Announcements End---------------------------------- -->
+    <!--  -->
+    <!------------------------------footer Section Start---------------------------------------->
+    <footer>
+        <div class="container1">
+            <div class="footer-content-box">
+                <h2 class="footer-headerTag">About</h2>
+                <p class="para-educator">Providing Life Changing Experiences Through Education. Class That Fit Your Busy Life. Closer toHome</p>
+                <ul class="footer-contact">
+                    <li class="footer-contact-list">
+                        <a href="#"><i class="fa-solid fa-phone"></i>&nbsp;&nbsp;1-232-432-4432</a>
+                    </li>
+                    <li class="footer-contact-list">
+                        <a href="#"><i class="fa-regular fa-clock"></i> &nbsp;&nbsp;Mon - Sat 8:00 - 18:00</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h2 class="footer-headerTag">Latest News</h2>
+                <ul class="footer-contact">
+                    <li class="footer-contact-list"><a href="#">Graduate Admissions</a></li>
+                    <li class="footer-contact-list">Admission open</li>
+                    <li class="footer-contact-list"><a href="#">Continuing Education</a></li>
+                    <li class="footer-contact-list">by Craig Murphy</li>
+                    <li class="footer-contact-list"><a href="#">Current Students</a></li>
+                    <li class="footer-contact-list">by Craig Murphy</li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h2 class="footer-headerTag">Useful Links</h2>
+                <ul class="footer-contact">
+                    <li class="footer-contact-list"><a href="#">Popular Courses</a></li>
+                    <li class="footer-contact-list"><a href="#">Graduate Admissions</a></li>
+                    <li class="footer-contact-list"><a href="#">Our Teachers</a></li>
+                    <li class="footer-contact-list"><a href="#">Upcoming Events New</a></li>
+                    <li class="footer-contact-list"><a href="#"> Contact-Us</a></li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h2 class="footer-headerTag">Flexible learning</h2>
+                <ul class="footer-contact">
+                    <li class="footer-contact-list"><a href="#"><img src="images/footer-map-2.png" class="footer-img" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="container horigentalTag">
+            
+        </div>
+    </footer>
+<!-- -----------------------------------------Footer Section End---------------------------------------- -->
+    <script src="js/all.min.js"></script>
+    <script>
+        let slider=["images/college2.jpg","images/college3.jpg","images/college4.jpg","images/college5.jpg","images/college6.jpg"];
+        let i=0;
+        function demo(){
+            let im=document.getElementById("img");
+            im.src=slider[i];
+            i++;
+            if(i==slider.length){
+                i=0;
+            }
+        }
+        window.setInterval(demo,2000);
+    </script>
+</body>
+</html>
