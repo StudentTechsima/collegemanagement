1589a062d2ade84e70a4801497acf0144f488c05
abhishek1561 <ap2043504@gmail.com>
2024-01-29 19:40:53 +0530

    first modify

---
+++ footer.php
@@ -0,0 +1,52 @@
+<!-- <?php error_reporting(0); ?> -->
+<!------------------------------footer Section Start---------------------------------------->
+<footer>
+        <div class="container1">
+            <div class="footer-content-box">
+                <h2 class="footer-headerTag">About</h2>
+                <p class="para-educator">Providing Life Changing Experiences Through Education. Class That Fit Your Busy Life. Closer toHome</p>
+                <ul class="footer-contact">
+                    <li class="footer-contact-list">
+                        <a href="#"><i class="fa-solid fa-phone"></i>&nbsp;&nbsp;1-232-432-4432</a>
+                    </li>
+                    <li class="footer-contact-list">
+                        <a href="#"><i class="fa-regular fa-clock"></i> &nbsp;&nbsp;Mon - Sat 8:00 - 18:00</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h2 class="footer-headerTag">Latest News</h2>
+                <ul class="footer-contact">
+                    <li class="footer-contact-list"><a href="#">Graduate Admissions</a></li>
+                    <li class="footer-contact-list">Admission open</li>
+                    <li class="footer-contact-list"><a href="#">Continuing Education</a></li>
+                    <li class="footer-contact-list">by Craig Murphy</li>
+                    <li class="footer-contact-list"><a href="#">Current Students</a></li>
+                    <li class="footer-contact-list">by Craig Murphy</li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h2 class="footer-headerTag">Useful Links</h2>
+                <ul class="footer-contact">
+                    <li class="footer-contact-list"><a href="#">Popular Courses</a></li>
+                    <li class="footer-contact-list"><a href="#">Graduate Admissions</a></li>
+                    <li class="footer-contact-list"><a href="#">Our Teachers</a></li>
+                    <li class="footer-contact-list"><a href="#">Upcoming Events New</a></li>
+                    <li class="footer-contact-list"><a href="#"> Contact-Us</a></li>
+                </ul>
+            </div>
+            <div class="footer-content-box">
+                <h2 class="footer-headerTag">Flexible learning</h2>
+                <ul class="footer-contact">
+                    <li class="footer-contact-list"><a href="#"><img src="images/footer-map-2.png" class="footer-img" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="container horigentalTag">
+            
+        </div>
+    </footer>
+<!-- -----------------------------------------Footer Section End---------------------------------------- -->
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School Management</title>
+    <link rel="icon" href="images/managment-software-2048x2048.png">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/all.css">
+</head>
+<body>
+    <script src="js/fontawesome.min.js"></script>
+    <!-------------------------------------Header Section Start--------------------------------------------->
+    <header>
+        <div class="contect"><a href=""><i class="fa-solid fa-phone fa-spin"></i>&nbsp;Helpline:+91 7800996687</a></div>
+        <div class="contect1"><a href=""><i class="fa-regular fa-envelope"></i>&nbsp;schoolmanagement@gmail.com</a></div>
+        <div class="contect2">
+            <div class="social-contect facebook"><span><a href="" class="social-contact"><i class="fa-brands fa-facebook"></i></a></span></div>
+            <div class="social-contect instagram"><span><a href="" class="social-contact"><i class="fa-brands fa-instagram"></i></a></span></div>
+            <div class="social-contect youtube"><span><a href="" class="social-contact"><i class="fa-brands fa-youtube"></i></a></span></div>
+        </div>
+        <div class="contect3"><a href="form.php">LOGIN</a></div>
+    </header>
+    <!-----------------------------Header Section End-------------------------------------->
+
+    <!------------------------------Logo Section Start-------------------------------->
+    <section>
+        <div class="container">
+            <div class="logo">
+                <img src="images/managment-software-2048x2048.png" alt="">
+            </div>
+            <div class="schoolname">
+                <h1>SCHOOL MANAGEMENT</h1><br>
+                <h2>स्कूल मैनेजमेंट </h2>
+            </div>
+            <div class="logo1">
+                <img src="images/588030_87d9a.gif" alt="">
+            </div>
+        </div>
+    </section>
+    <!--------------------------------Logo Section End---------------------------------->
+
+    <!----------------------------------Landing Section Start----------------------------------->
+    <section>
+        <div class="manubar">
+            <ul>
+                <a href="" class="home"><li class="link home">HOME</li></a>
+                <a href=""><li class="link">SCHOOLS</li></a>
+               
+                <a href=""><li class="link">REGISTERS</li></a>
+                <a href="department.html"><li class="link">TEACHERS</li></a>
+                <a href=""><li class="link">NEWS</li></a>
+                <a href=""><li class="link">COURSES</li></a>
+                <a href=""><li class="link">ABOUT US</li></a>
+                <a href=""><li class="link">GALLERY</li></a>
+                <a href=""><li class="link">FAQ</li></a>
+                <a href=""><li class="link">CONTACT US</li></a>
+            </ul>
+            <div class="manubar1"><a href=""><div class="manudiv"></div></a>
+        </div>
+        </div>
+    </section>    
+    <!----------------------------------Landing Section End----------------------------------->
